134542bddc707c00172857d120d6ec4e641616a6
Danchuk-Lauzon, Alexy <adanchuklauzon@lcs-mba3080.lcs.on.ca>
2018-04-18 10:55:58 -0400

    From 1987 to 2013 V.1 (finished)
    10000 bug

--- From1987to2013/main.swift
+++ From1987to2013/main.swift
@@ -8,10 +8,90 @@
 
 import Foundation
 
-// Get the user input
-var rawInput = readLine()
 
-// Print out the input provided
-print("You said:")
-print(rawInput)
+
+
+var validInput = 0
+while 1 == 1 {
+    
+    guard let input = readLine() else {
+        continue
+    }
+    
+    guard let intInput = Int(input) else {
+        continue
+    }
+    
+    if intInput < 0 || intInput > 10000 {
+        continue
+    }
+    
+    validInput = intInput
+    break
+}
+
+
+var nextYear = validInput + 1
+while 1 == 1 {
+    
+    var digit1 = 0
+    var processYear = 0
+    if nextYear / 1000 < 10 {
+        digit1 = nextYear / 1000
+        processYear = nextYear % 1000
+        
+    } else {
+        
+    }
+    
+    let digit2 = processYear / 100
+    processYear = processYear % 100
+    
+    let digit3 = processYear / 10
+    processYear = processYear % 10
+    
+    let digit4 = processYear / 1
+    
+    
+    if digit1 == digit2 || digit1 == digit3 || digit1 == digit4 {
+      nextYear += 1
+      continue
+    }
+   
+    if digit2 == digit1 || digit2 == digit3 || digit2 == digit4 {
+        nextYear += 1
+        continue
+    }
+    
+    if digit3 == digit1 || digit3 == digit2 || digit3 == digit4 {
+        nextYear += 1
+        continue
+    }
+    
+    if digit4 == digit1 || digit4 == digit2 || digit4 == digit3 {
+        nextYear += 1
+        continue
+    }
+    
+    print("The next year with distinct digits is \(nextYear)")
+    break
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
